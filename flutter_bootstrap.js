3fc230f04e5a61f8dfc50ed9ed6872f14b5e85e6
Teddy903 <samik903@gmail.com>
2026-08-20 20:06:38 +0300

    5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1533376305" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3987269152" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
